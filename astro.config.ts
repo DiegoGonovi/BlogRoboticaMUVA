57582dbbc9b3d894e5d195204e983ce45e024066
DiegoGonovi <145789136+DiegoGonovi@users.noreply.github.com>
2025-04-12 04:57:11 +0200

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -116,7 +116,7 @@ export default defineConfig({
   prefetch: true,
   // ! Please remember to replace the following site property with your own domain
   site: "http://https://diegogonovi.github.io",
-  base: "BlogRoboticaMUVA",
+  base: "/BlogRoboticaMUVA",
   vite: {
     build: {
       sourcemap: true, // Source maps generation
