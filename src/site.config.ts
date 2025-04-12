8f4006e545fb3dcf43099f88e1473bfe986dffc2
DiegoGonovi <145789136+DiegoGonovi@users.noreply.github.com>
2025-04-12 04:41:02 +0200

    Add site

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,10 +2,10 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Artem Kutsan",
+	author: "Diego González Oviaño",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
-		locale: "en-GB",
+		locale: "es-ES",
 		options: {
 			day: "numeric",
 			month: "short",
@@ -15,29 +15,29 @@ export const siteConfig: SiteConfig = {
 	// Used as the default description meta property and webmanifest description
 	description: "An opinionated starter theme for Astro",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
-	lang: "en-GB",
+	lang: "es-ES",
 	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_GB",
+	ogLocale: "es-ES",
 	// Used to construct the meta title property found in src/components/BaseHead.astro L:11, and webmanifest name found in astro.config.ts L:42
-	title: "Citrus",
+	title: "Blog Robótica",
 };
 
 // Used to generate links in both the Header & Footer.
 export const menuLinks: { path: string; title: string }[] = [
 	{
-		path: "/",
+		path: "/BlogRoboticaMUVA/",
 		title: "Home",
 	},
 	{
-		path: "/about/",
+		path: "/BlogRoboticaMUVA/about/",
 		title: "About",
 	},
 	{
-		path: "/posts/",
+		path: "/BlogRoboticaMUVA/posts/",
 		title: "Blog",
 	},
 	{
-		path: "/notes/",
+		path: "/BlogRoboticaMUVA/notes/",
 		title: "Notes",
 	},
 ];
